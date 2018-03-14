c8de456f002969f6d6cedfc4ef7c7d8a603512d1
pdawson2090 <36117746+pdawson2090@users.noreply.github.com>
2018-03-13 21:51:00 -0400

    gotime

---
+++ demo/src/main/java/com/livelife/livelife/LivelifeApplication.java
@@ -0,0 +1,12 @@
+package com.livelife.livelife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivelifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivelifeApplication.class, args);
+	}
+}
